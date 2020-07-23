c615291451013c64a1c9cafac4c8db2562cb140a
James Grigoras <jamesgrigoras@yahoo.com>
2020-07-22 21:57:02 -0400

    CORS

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,18 +3,19 @@ const User = require('../models/user');
 const passport = require('passport');
 
 const authenticate = require('../authenticate');
+const cors = require('./cors')
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     User.find()
     .then(users => {
         res.status(200).json(users);
     })
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
     User.register(
         new User({username: req.body.username}),
         req.body.password,
@@ -47,7 +48,7 @@ router.post('/signup', (req, res) => {
     );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({_id: req.user._id})
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -56,7 +57,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 
 
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
     if (req.session) {
       req.session.destroy();
       res.clearCookie('session-id');
